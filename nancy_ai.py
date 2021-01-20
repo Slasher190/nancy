c976aed9c1b1cb041089728e96bf86ecc1ca94fe
Slasher190 <57698770+Slasher190@users.noreply.github.com>
2021-01-20 20:09:39 +0530

    Update nancy_ai.py

--- nancy_ai.py
+++ nancy_ai.py
@@ -15,7 +15,38 @@ engine.setProperty('voice', voices[1].id)
 def speak(audio):
     engine.say(audio)
     engine.runAndWait()
-
+    
+def takeCommandHindi(): 
+         
+    r = sr.Recognizer() 
+    with sr.Microphone() as source: 
+          
+        # seconds of non-speaking audio before  
+        # a phrase is considered complete 
+        print('Listening') 
+        r.pause_threshold = 0.7  
+        audio = r.listen(source)   
+        try: 
+            print("Recognizing") 
+            Query = r.recognize_google(audio, language='hi-In') 
+              
+            # for listening the command in indian english 
+            print("the query is printed='", Query, "'") 
+          
+        # handling the exception, so that assistant can  
+        # ask for telling again the command 
+        except Exception as e: 
+            print(e)   
+            print("Say that again sir") 
+            return "None"
+        return Query 
+  
+  takeCommandHindi() 
+  
+# Driver Code 
+           
+# call the function 
+ 
 def ask():
     # Record Audio
     r = sr.Recognizer()
